552fa064bced0bf1b7c0d0e3bdacd60bd93ef92b
Baptiste Rimetz <b.rimetz@student.isartdigital.com>
2020-09-07 18:17:03 +0200

    Zoom Before Test

--- Patte d'Ours/Assets/Scripts/TouchHandler.cs
+++ Patte d'Ours/Assets/Scripts/TouchHandler.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class TouchHandler : MonoBehaviour
@@ -11,10 +13,19 @@ public class TouchHandler : MonoBehaviour
     bool IsOnTouch = false;
 
     private float m_touchDuration = 0f;
-    private float m_durationForAnImpulse = 0.25f;
+    //private float m_durationForASwipe = 0.25f;
 
     private Vector3 m_firstTouchPosition = Vector3.zero;
     private Vector3 m_currentTouchPosition = Vector3.zero;
+
+    #endregion
+
+    #region ZoomVars
+
+    [SerializeField] Transform cameraTarget = null;
+    [SerializeField] Transform cameraTransform = null;
+
+    private float PreviousDistanceBetweenTouch = 0.0f;
 
     #endregion
 
@@ -48,6 +59,9 @@ public class TouchHandler : MonoBehaviour
     #region TouchFunctions
     void CheckTouch()
     {
+        if (Input.touchCount == 2)
+            UpdateTouchZoom();
+
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
@@ -69,6 +83,30 @@ public class TouchHandler : MonoBehaviour
                 default:
                     break;
             }
+        }
+    }
+
+    void UpdateTouchZoom()
+    {
+        Touch touch_first = Input.GetTouch(0);
+        Touch touch_second = Input.GetTouch(1);
+
+        switch (touch_first.phase)
+        {
+            case TouchPhase.Began:
+                IsOnTouch = true;
+                break;
+            case TouchPhase.Moved:
+                TouchZoomMoved(ref touch_first, ref touch_second);
+                break;
+            case TouchPhase.Ended:
+                EndZoomTouch();
+                break;
+            case TouchPhase.Canceled:
+                EndZoomTouch();
+                break;
+            default:
+                break;
         }
     }
 
@@ -98,6 +136,35 @@ public class TouchHandler : MonoBehaviour
         m_firstTouchPosition = Vector3.zero;
 
         //cubeControlled.EndTouch();
+    }
+
+
+    void TouchZoomMoved(ref Touch firstTouch, ref Touch secondTouch)
+    {
+        Vector3 tmpVec = firstTouch.position - secondTouch.position;
+        float currentDistance = tmpVec.sqrMagnitude;
+
+        MoveZoomCamera(currentDistance);
+
+        PreviousDistanceBetweenTouch = currentDistance;
+    }
+
+    void MoveZoomCamera(float currentDistance)
+    {
+        Vector3 UnitVec = cameraTarget.position - cameraTransform.position;
+        UnitVec = Vector3.Normalize(UnitVec);
+
+        cameraTransform.position = cameraTransform.position + Time.deltaTime * (currentDistance-PreviousDistanceBetweenTouch) * UnitVec;
+    }
+
+    void EndZoomTouch()
+    {
+        // Reset Touch Variables
+        IsOnTouch = false;
+        m_touchDuration = 0f;
+        m_firstTouchPosition = Vector3.zero;
+
+        PreviousDistanceBetweenTouch = 0.0f;
     }
     #endregion
 }

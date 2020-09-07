daa4cbeb85c7e182a1ccffa9cb3c647ec2e28ba9
Baptiste Rimetz <b.rimetz@student.isartdigital.com>
2020-09-07 23:25:13 +0200

    Lock Vertical, TargetFrameRate at 62, Swipe and Zoom In and Out, and print FPS

--- Patte d'Ours/Assets/Scripts/TouchHandler.cs
+++ Patte d'Ours/Assets/Scripts/TouchHandler.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Globalization;
 using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TouchHandler : MonoBehaviour
 {
     //[SerializeField] CubeBehavior cubeControlled = null;
+
+    public Text text = null;
+    public Text text2 = null;
+    public Text text3 = null;
 
     #region TouchVars
 
@@ -17,6 +23,14 @@ public class TouchHandler : MonoBehaviour
 
     private Vector3 m_firstTouchPosition = Vector3.zero;
     private Vector3 m_currentTouchPosition = Vector3.zero;
+    private Vector3 m_previousTouchPosition = Vector3.zero;
+    public float length = 0;
+    #endregion
+
+    #region SwipeVars
+
+    Touch previousTouch;
+    float rotSpeed = 4f;
 
     #endregion
 
@@ -25,15 +39,23 @@ public class TouchHandler : MonoBehaviour
     [SerializeField] Transform cameraTarget = null;
     [SerializeField] Transform cameraTransform = null;
 
-    private float PreviousDistanceBetweenTouch = 0.0f;
+    public float ZoomSpeed = 0.0001f;
 
     #endregion
+
+    void Start()
+    {
+        Application.targetFrameRate = 62;
+    }
 
     // Update is called once per frame
     void Update()
     {
         CheckTouch();
         CheckTimer();
+
+        Vector3 UnitVec = cameraTarget.position - cameraTransform.position;
+        length = UnitVec.magnitude;
     }
 
     #region TimerFunction
@@ -61,110 +83,66 @@ public class TouchHandler : MonoBehaviour
     {
         if (Input.touchCount == 2)
             UpdateTouchZoom();
-
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-
-            switch (touch.phase)
-            {
-                case TouchPhase.Began:
-                    IsOnTouch = true;
-                    break;
-                case TouchPhase.Moved:
-                    TouchMoved(ref touch);
-                    break;
-                case TouchPhase.Ended:
-                    TouchEnded(ref touch);
-                    break;
-                case TouchPhase.Canceled:
-                    EndTouch();
-                    break;
-                default:
-                    break;
-            }
-        }
+        else if (Input.touchCount > 0)
+            UpdateTouchSwipe();
     }
 
-    void UpdateTouchZoom()
+    void UpdateTouchSwipe()
     {
-        Touch touch_first = Input.GetTouch(0);
-        Touch touch_second = Input.GetTouch(1);
+        Touch touch = Input.GetTouch(0);
 
-        switch (touch_first.phase)
+        switch (touch.phase)
         {
             case TouchPhase.Began:
                 IsOnTouch = true;
+                previousTouch = touch;
                 break;
             case TouchPhase.Moved:
-                TouchZoomMoved(ref touch_first, ref touch_second);
-                break;
-            case TouchPhase.Ended:
-                EndZoomTouch();
-                break;
-            case TouchPhase.Canceled:
-                EndZoomTouch();
+                RotateCamera(ref touch);
                 break;
             default:
                 break;
         }
     }
 
-    void TouchMoved(ref Touch touch)
+    void RotateCamera(ref Touch touch)
     {
-        m_currentTouchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-
-        if (m_firstTouchPosition == Vector3.zero)
-            m_firstTouchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+        float deltaX = previousTouch.position.x - touch.position.x;
+        float rotY = -deltaX * Time.deltaTime * rotSpeed;
+        text.text = rotY.ToString();
+        cameraTarget.eulerAngles = new Vector3(cameraTarget.eulerAngles.x, cameraTarget.eulerAngles.y + rotY);
+        previousTouch = touch;
     }
 
-    void TouchEnded(ref Touch touch)
+    void UpdateTouchZoom()
     {
-        /*if (m_touchDuration <= m_durationForAnImpulse)
-            cubeControlled.Jump(ref m_currentTouchPosition);
-        else
-            cubeControlled.Propulse();*/
+        float touchesPrevPosDifference = 0; float touchesCurPosDifference = 0;
+        float zoomModifier = CalcZoomModifier(ref touchesPrevPosDifference, ref touchesCurPosDifference);
 
-        EndTouch();
-    }
-
-    void EndTouch()
-    {
-        // Reset Touch Variables
-        IsOnTouch = false;
-        m_touchDuration = 0f;
-        m_firstTouchPosition = Vector3.zero;
-
-        //cubeControlled.EndTouch();
-    }
-
-
-    void TouchZoomMoved(ref Touch firstTouch, ref Touch secondTouch)
-    {
-        Vector3 tmpVec = firstTouch.position - secondTouch.position;
-        float currentDistance = tmpVec.sqrMagnitude;
-
-        MoveZoomCamera(currentDistance);
-
-        PreviousDistanceBetweenTouch = currentDistance;
-    }
-
-    void MoveZoomCamera(float currentDistance)
-    {
         Vector3 UnitVec = cameraTarget.position - cameraTransform.position;
+        length = UnitVec.magnitude;
         UnitVec = Vector3.Normalize(UnitVec);
 
-        cameraTransform.position = cameraTransform.position + Time.deltaTime * (currentDistance-PreviousDistanceBetweenTouch) * UnitVec;
+        // Move and check min and max of zoom
+        if (touchesPrevPosDifference > touchesCurPosDifference && length < 50)
+            cameraTransform.position = cameraTransform.position - zoomModifier * UnitVec;
+        else if (touchesPrevPosDifference < touchesCurPosDifference && length > 15)
+            cameraTransform.position = cameraTransform.position + zoomModifier * UnitVec;
     }
 
-    void EndZoomTouch()
+    float CalcZoomModifier(ref float touchesPrevPosDifference, ref float touchesCurPosDifference)
     {
-        // Reset Touch Variables
-        IsOnTouch = false;
-        m_touchDuration = 0f;
-        m_firstTouchPosition = Vector3.zero;
+        Touch firstTouch = Input.GetTouch(0);
+        Touch secondTouch = Input.GetTouch(1);
 
-        PreviousDistanceBetweenTouch = 0.0f;
+        Vector3 firstTouchPrevPos = firstTouch.position - firstTouch.deltaPosition;
+        Vector3 secondTouchPrevPos = secondTouch.position - secondTouch.deltaPosition;
+
+        touchesPrevPosDifference = (firstTouchPrevPos - secondTouchPrevPos).magnitude;
+        touchesCurPosDifference = (firstTouch.position - secondTouch.position).magnitude;
+
+        return (firstTouch.deltaPosition - secondTouch.deltaPosition).magnitude * ZoomSpeed;
     }
+
     #endregion
 }

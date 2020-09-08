389fca11f9377f6867d7618d0c325cff07e8965c
Baptiste Rimetz <b.rimetz@student.isartdigital.com>
2020-09-08 12:21:56 +0200

    Rework IA In order to work with Future Game Func

--- Patte d'Ours/Assets/Scripts/PolarBear.cs
+++ Patte d'Ours/Assets/Scripts/PolarBear.cs
@@ -4,57 +4,60 @@ using UnityEngine;
 
 public class PolarBear : MonoBehaviour
 {
-    public List<Transform> Waypoints = new List<Transform>();
-
-    Transform TargetWaypoint = null;
-    int indexTargetWaypoint = 0;
+    public Transform targetWaypoint = null;
 
     public float minDistance = 0.1f;
-    int lastWaypointIndex;
 
     public float movementSpeed = 2.0f;
 
     public float rotationSpeed = 2.0f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        lastWaypointIndex = Waypoints.Count;
-        TargetWaypoint = Waypoints[indexTargetWaypoint];
-    }
+    public int chanceToDoAction = 80;
+    bool actionLauch = false;
+
+    public bool needNewWaypoint = false;
 
     // Update is called once per frame
     void Update()
     {
-        float movementStep = movementSpeed * Time.deltaTime;
+        if (actionLauch)
+        {
+            CheckActionFinish();
+        }
+        else if (!needNewWaypoint)
+        {
+            float movementStep = movementSpeed * Time.deltaTime;
 
-        float rotationStep = rotationSpeed * Time.deltaTime;
+            float rotationStep = rotationSpeed * Time.deltaTime;
 
-        Vector3 directionToTarget = TargetWaypoint.position - transform.position;
-        Quaternion rotationToTarget = Quaternion.LookRotation(directionToTarget);
+            Vector3 directionToTarget = targetWaypoint.position - transform.position;
+            Quaternion rotationToTarget = Quaternion.LookRotation(directionToTarget);
 
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotationToTarget, rotationStep);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotationToTarget, rotationStep);
 
-        float distance = Vector3.Distance(transform.position, TargetWaypoint.position);
-        CheckDistanceToWaypoint(distance);
+            float distance = Vector3.Distance(transform.position, targetWaypoint.position);
+            CheckDistanceToWaypoint(distance);
 
-        transform.position = Vector3.MoveTowards(transform.position, TargetWaypoint.position, movementStep);
-
+            transform.position = Vector3.MoveTowards(transform.position, targetWaypoint.position, movementStep);
+        }
     }
 
     void CheckDistanceToWaypoint(float currentDistance)
     {
         if (currentDistance  <= minDistance)
         {
-            indexTargetWaypoint++;
-            if (indexTargetWaypoint >= lastWaypointIndex)
-                indexTargetWaypoint = 0;
-            UpdateTargetWaypoint();
+            needNewWaypoint = true;
+
+            if (Random.Range(1, 100) <= chanceToDoAction)
+            {
+                // launch Action
+                actionLauch = true;
+            }
         }
     }
 
-    void UpdateTargetWaypoint()
+    void CheckActionFinish()
     {
-        TargetWaypoint = Waypoints[indexTargetWaypoint];
+
     }
 }

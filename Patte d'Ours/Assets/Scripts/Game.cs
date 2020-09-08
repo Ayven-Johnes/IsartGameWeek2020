0a5ce9f0393f97d399c9d4812a3f86e38ab734b4
Johan <jo.menard@student.isartdigital.com>
2020-09-08 12:25:15 +0200

    Add Hearts Management / Shopping Menu

---
+++ Patte d'Ours/Assets/Scripts/Game.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour
+{ 
+    [SerializeField]
+    private int     HeartsPerSecond = 0;
+    private float   Hearts = 0;
+
+    [SerializeField]
+    private Text HeartsDisplay = null;
+    [SerializeField]
+    private Text HeartsPerSecDisplay = null;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Find the previous player's score inside Unity's player pref.
+        Hearts = PlayerPrefs.GetInt("Hearts");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateHearts();
+
+        // Debugging Logs
+        Debug.Log((int)Hearts);
+        Debug.Log(HeartsPerSecond);
+    }
+
+    private void UpdateHearts()
+    {
+        HeartsDisplay.text = "Hearts : " + ((int)Hearts).ToString();
+        HeartsPerSecDisplay.text = "Hearts per second : " + HeartsPerSecond.ToString();
+
+        Hearts += HeartsPerSecond * Time.deltaTime;
+
+        // Save the player's "score" inside Unity's player pref.
+        PlayerPrefs.SetInt("Hearts", (int)Hearts);
+    }
+
+    public bool Buy(int cost)
+    {
+        if(cost > 0 && cost <= Hearts)
+        {
+            Hearts -= cost;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
